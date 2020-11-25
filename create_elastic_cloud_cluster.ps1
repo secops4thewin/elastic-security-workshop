d94480019b5d3ae4248f11d8665dfe8a87888199
Mickey <Mickey@elastic[.]co>
2020-11-25 12:20:51 +1100

    Updated

--- create_elastic_cloud_cluster.ps1
+++ create_elastic_cloud_cluster.ps1
@@ -1,9 +1,9 @@
 param (
-    [string]$api_key = $(throw "-api_key is required."),
-    [string]$target_gcp_region = $(throw "-target_gcp_region is required."),
-    [string]$cluster_name = $(throw "-cluster_name is required."),
-    [string]$stack_version = $(throw "-stack_version is required."),
-    [string]$agent_version = $(throw "-agent_version is required.")
+[string]$api_key = $(throw "-api_key is required."),
+[string]$target_gcp_region = $(throw "-target_gcp_region is required."),
+[string]$cluster_name = $(throw "-cluster_name is required."),
+[string]$stack_version = $(throw "-stack_version is required."),
+[string]$agent_version = $(throw "-agent_version is required.")
  )
 
  ## Start Cluster Deployment section
@@ -31,7 +31,7 @@ $sysmon_local_rules_filepath = "C:\Windows\sysmon.xml"
 Write-Output "Installing Sysmon..."
 $sysmon_tmp_dir = "$install_dir\sysmon"
 if (!(Test-Path $sysmon_tmp_dir)) {
-    New-Item -Path $sysmon_tmp_dir -Type directory | Out-Null
+New-Item -Path $sysmon_tmp_dir -Type directory | Out-Null
 }
 Invoke-WebRequest -Uri $sysmon_config_uri -OutFile $sysmon_local_rules_filepath
 Invoke-WebRequest -Uri $sysmon_installer_uri -OutFile $sysmon_tmp_dir/Sysmon.zip
@@ -42,7 +42,7 @@ Write-Output "Sysmon Installation Complete"
 
 #Download Elastic Cloud Deployment Plan
 Write-Output "`nDownloading Elastic Cloud Deployment Plan..."
-Invoke-WebRequest -Uri "$wsplan_config_respository_uri/wsplan.json" -OutFile "$install_dir\wsplan.json"    
+Invoke-WebRequest -Uri "$wsplan_config_respository_uri/wsplan.json" -OutFile "$install_dir\wsplan.json"
 
 
 #Update Elastic Cloud Plan based on command line parameters
@@ -53,20 +53,20 @@ $elastic_cloud_plan.resources[0].elasticsearch[0].plan.elasticsearch.version = $
 $elastic_cloud_plan.resources[0].kibana[0].plan.kibana.version = $stack_version
 
 $cluster_info = Invoke-RestMethod -Method Post -Uri $elastic_cloud_api_uri `
-                                  -Headers @{ 'Authorization' = "ApiKey $api_key"; 'Content-Type' = 'application/json'} `
-                                  -Body ($elastic_cloud_plan | ConvertTo-Json -Depth 32)
+  -Headers @{ 'Authorization' = "ApiKey $api_key"; 'Content-Type' = 'application/json'} `
+  -Body ($elastic_cloud_plan | ConvertTo-Json -Depth 32)
 
-$cluster_id = $cluster_info.id 
+$cluster_id = $cluster_info.id
 $cloud_id = $cluster_info.resources.cloud_id
 $password = $cluster_info.resources.credentials.password
 
 Write-Output "Elastic Cloud Deployment [$cluster_id] is being created"
 do {
-    Start-Sleep -Seconds 10
-    $cluster = (Invoke-RestMethod -Method Get -Uri $elastic_cloud_api_uri/$cluster_id `
-                                  -Headers @{ 'Authorization' = "ApiKey $api_key"; 'Content-Type' = 'application/json'})
-    $healthy = $cluster.healthy
-    Write-Host -NoNewLine "."
+Start-Sleep -Seconds 10
+$cluster = (Invoke-RestMethod -Method Get -Uri $elastic_cloud_api_uri/$cluster_id `
+  -Headers @{ 'Authorization' = "ApiKey $api_key"; 'Content-Type' = 'application/json'})
+$healthy = $cluster.healthy
+Write-Host -NoNewLine "."
 }
 until ($healthy -eq $True)
 Write-Output "`nElastic Cloud Deployment was created successfully!"
@@ -87,52 +87,52 @@ Write-Output "Uninstalling existing beats"
 #Uninstall all Elastic Beats already installed
 $app = Get-WmiObject -Class Win32_Product -Filter ("Vendor = 'Elastic'")
 if ($null -ne $app) {
-    $app.Uninstall()
+$app.Uninstall()
 }
 
 #Configure Beats
 function ElasticBeatSetup ([string]$beat_name)
 {
-    Write-Output "`n*** Setting up $beat_name ****"
-    $beat_install_folder = "C:\Program Files\Elastic\Beats\$agent_version\$beat_name"
-    $beat_exe_path = "$beat_install_folder\$beat_name.exe"
-    $beat_config_path = "C:\ProgramData\Elastic\Beats\$beat_name\$beat_name.yml"
-    $beat_data_path = "C:\ProgramData\Elastic\Beats\$beat_name\data"
-    $beat_config_file = "$beat_config_repository_url/$beatname.yml"
-    $beat_artifact_uri = "https://artifacts.elastic.co/downloads/beats/$beat_name/$beat_name-$agent_version-windows-x86_64.msi"
-    $log_file_path = "$install_dir\$beat_name.log"
+Write-Output "`n*** Setting up $beat_name ****"
+$beat_install_folder = "C:\Program Files\Elastic\Beats\$agent_version\$beat_name"
+$beat_exe_path = "$beat_install_folder\$beat_name.exe"
+$beat_config_path = "C:\ProgramData\Elastic\Beats\$beat_name\$beat_name.yml"
+$beat_data_path = "C:\ProgramData\Elastic\Beats\$beat_name\data"
+$beat_config_file = "$beat_config_repository_url/$beatname.yml"
+$beat_artifact_uri = "https://artifacts.elastic.co/downloads/beats/$beat_name/$beat_name-$agent_version-windows-x86_64.msi"
+$log_file_path = "$install_dir\$beat_name.log"
 
-    Write-Output "Installing $beat_name..."
-    Invoke-WebRequest -Uri "$beat_artifact_uri" -OutFile "$install_dir\$beat_name-$agent_version-windows-x86_64.msi"
-    $MSIArguments = @(
-        "/i"
-        "$install_dir\$beat_name-$agent_version-windows-x86_64.msi"
-        "/qn"
-        "/norestart"
-        "/L"
-        $log_file_path
-    )
-    Start-Process msiexec.exe -Wait -ArgumentList $MSIArguments -NoNewWindow
+Write-Output "Installing $beat_name..."
+Invoke-WebRequest -Uri "$beat_artifact_uri" -OutFile "$install_dir\$beat_name-$agent_version-windows-x86_64.msi"
+$MSIArguments = @(
+"/i"
+"$install_dir\$beat_name-$agent_version-windows-x86_64.msi"
+"/qn"
+"/norestart"
+"/L"
+$log_file_path
+)
+Start-Process msiexec.exe -Wait -ArgumentList $MSIArguments -NoNewWindow
 
-    #Download Beat configuration file
-    Invoke-WebRequest -Uri "$beat_config_repository_uri/$beat_name.yml" -OutFile $beat_config_path
+#Download Beat configuration file
+Invoke-WebRequest -Uri "$beat_config_repository_uri/$beat_name.yml" -OutFile $beat_config_path
 
-    # Create Beat Keystore and add CLOUD_ID and ES_PWD keys to it
-    $params = $('-c', $beat_config_path, 'keystore','create','--force')
-    & $beat_exe_path @params
-    $params = $('-c', $beat_config_path, 'keystore','add','CLOUD_ID','--stdin','--force','-path.data', $beat_data_path)
-    Write-Output $cloud_id | & $beat_exe_path @params
-    $params = $('-c', $beat_config_path, 'keystore','add','ES_PWD','--stdin','--force','-path.data', $beat_data_path)
-    Write-Output $password | & $beat_exe_path @params
-    
-    # Run Beat Setup
-    Write-Output "Running $beat_name setup..."
-    $params = $('-c', $beat_config_path, 'setup', '-path.data', $beat_data_path)
-    & $beat_exe_path @params
+# Create Beat Keystore and add CLOUD_ID and ES_PWD keys to it
+$params = $('-c', $beat_config_path, 'keystore','create','--force')
+& $beat_exe_path @params
+$params = $('-c', $beat_config_path, 'keystore','add','CLOUD_ID','--stdin','--force','-path.data', $beat_data_path)
+Write-Output $cloud_id | & $beat_exe_path @params
+$params = $('-c', $beat_config_path, 'keystore','add','ES_PWD','--stdin','--force','-path.data', $beat_data_path)
+Write-Output $password | & $beat_exe_path @params
 
-    Write-Output "Starting $beat_name Service"
-    Start-Service -Name $beat_name
-    & sc.exe failure $beat_name reset=30 actions=restart/5000
+# Run Beat Setup
+Write-Output "Running $beat_name setup..."
+$params = $('-c', $beat_config_path, 'setup', '-path.data', $beat_data_path)
+& $beat_exe_path @params
+
+Write-Output "Starting $beat_name Service"
+Start-Service -Name $beat_name
+& sc.exe failure $beat_name reset=30 actions=restart/5000
 }
 ElasticBeatSetup("winlogbeat");
 ElasticBeatSetup("packetbeat");
@@ -153,8 +153,8 @@ $credentialBytes = [System.Text.Encoding]::ASCII.GetBytes($credential)
 $base64Credential = [System.Convert]::ToBase64String($credentialBytes)
 $basicAuthHeader = "Basic $base64Credential"
 $headers = @{
-    "Authorization" = $basicAuthHeader;
-    "kbn-xsrf" = "reporting"
+"Authorization" = $basicAuthHeader;
+"kbn-xsrf" = "reporting"
 }
 $bodyMsg = @{"forceRecreate" = "true"}
 $bodyJson = ConvertTo-Json($bodyMsg)
@@ -185,27 +185,27 @@ Write-Output "Create Fleet User"
 Write-Output "Creating fleet user at https://$kibana_url/api/fleet/setup"
 $fleetCounter = 0
 do {
-    Start-Sleep -Seconds 20
-    Write-Output "Trying $fleetCounter times"
-    try{
-        Write-Output "Creating fleet user with POST request at https://$kibana_url/api/fleet/setup"
-    Invoke-WebRequest -UseBasicParsing -Uri  "https://$kibana_url/api/fleet/agents/setup" -ContentType "application/json" -Headers $headers -Method POST -body $bodyJson -ErrorAction SilentlyContinue -ErrorVariable SearchError
-    }
-    catch{
-        Write-output "Error Message Array: $searchError"
-    }
-    Start-Sleep -Seconds 5
-    # Checking the content output to see if the host is ready.
-    try{
-    Write-Output "Checking if Fleet Manager is ready with GET request https://$kibana_url/api/fleet/enrollment-api-keys?page=1&perPage=20"
-    $ekIDBody = (Invoke-WebRequest -UseBasicParsing -Uri  "https://$kibana_url/api/fleet/agent_policies?page=1&perPage=20&sortField=updated_at&sortOrder=desc&kuery=" -ContentType "application/json" -Headers $headers -Method GET  -ErrorVariable SearchError)
-    $isReady = (convertfrom-json($ekIDBody.content)).total
-    }
-    catch{
-        Write-output "Error Message Array: $searchError"
-    }
-    
-    $fleetCounter++
+Start-Sleep -Seconds 20
+Write-Output "Trying $fleetCounter times"
+try{
+Write-Output "Creating fleet user with POST request at https://$kibana_url/api/fleet/setup"
+Invoke-WebRequest -UseBasicParsing -Uri  "https://$kibana_url/api/fleet/agents/setup" -ContentType "application/json" -Headers $headers -Method POST -body $bodyJson -ErrorAction SilentlyContinue -ErrorVariable SearchError
+}
+catch{
+Write-output "Error Message Array: $searchError"
+}
+Start-Sleep -Seconds 5
+# Checking the content output to see if the host is ready.
+try{
+Write-Output "Checking if Fleet Manager is ready with GET request https://$kibana_url/api/fleet/enrollment-api-keys?page=1&perPage=20"
+$ekIDBody = (Invoke-WebRequest -UseBasicParsing -Uri  "https://$kibana_url/api/fleet/agent_policies?page=1&perPage=20&sortField=updated_at&sortOrder=desc&kuery=" -ContentType "application/json" -Headers $headers -Method GET  -ErrorVariable SearchError)
+$isReady = (convertfrom-json($ekIDBody.content)).total
+}
+catch{
+Write-output "Error Message Array: $searchError"
+}
+
+$fleetCounter++
 }
 until (($isReady -gt 0) -or ($fleetCounter -eq 5) )
 
@@ -224,21 +224,21 @@ $policyId = $ApiKeyActual.item[0].policy_id
 $packageList = (convertfrom-json(Invoke-WebRequest -UseBasicParsing -Uri  "https://$kibana_url/api/fleet/epm/packages" -ContentType "application/json" -Headers $headers -Method GET))
 $endpointPackageVersion = ($packageList.response | where {$_.name -eq "endpoint"}).version
 
-# Create a json request format suitable for  the configuration id 
+# Create a json request format suitable for  the configuration id
 $securityConfigDict = @"
 {
-    "name": "security",
-    "description": "",
-    "namespace": "default",
-    "policy_id": "$policyId",
-    "enabled": "true",
-    "output_id": "",
-    "inputs": [],
-    "package": {
-        "name": "endpoint",
-        "title": "Elastic Endpoint Security",
-        "version": "$endpointPackageVersion"
-    }
+"name": "security",
+"description": "",
+"namespace": "default",
+"policy_id": "$policyId",
+"enabled": "true",
+"output_id": "",
+"inputs": [],
+"package": {
+"name": "endpoint",
+"title": "Elastic Endpoint Security",
+"version": "$endpointPackageVersion"
+}
 }
 
 "@ | convertfrom-json
@@ -253,7 +253,7 @@ $elasticAgentUrl = "https://artifacts.elastic.co/downloads/beats/elastic-agent/e
 $agent_install_folder = "C:\Program Files\Elastic\Agent\"
 
 if (!(Test-Path $agent_install_folder)) {
-    New-Item -Path $agent_install_folder -Type directory | Out-Null
+New-Item -Path $agent_install_folder -Type directory | Out-Null
 }
 Write-Output "Downloading Elastic Agent"
 Invoke-WebRequest -Uri $elasticAgentUrl -OutFile "$install_dir\elastic-agent-$agent_version-windows-x86_64.zip"
@@ -273,7 +273,7 @@ if ((get-service "elastic-agent") -eq "Stopped")
 {
 Write-Output "Starting Agent Service"
 
-    start-service "elastic-agent"
+start-service "elastic-agent"
 }
 
 # Download Caldera Implant
@@ -282,10 +282,10 @@ Invoke-WebRequest -Uri "$workshop_uri/caldera_implant.ps1" -OutFile "C:\Users\Ad
 
 # Download Adversary Emulation Rules
 Write-Output "Downloading Workshop Rules"
-Invoke-WebRequest -Uri "$workshop_uri/siem_rules/AdversaryEmulation001.ndjson" -OutFile "$install_dir\AdversaryEmulation001.ndjson"    
-Invoke-WebRequest -Uri "$workshop_uri/siem_rules/AdversaryEmulation002.ndjson" -OutFile "$install_dir\AdversaryEmulation002.ndjson"    
-Invoke-WebRequest -Uri "$workshop_uri/siem_rules/AdversaryEmulation003.ndjson" -OutFile "$install_dir\AdversaryEmulation003.ndjson"    
-Invoke-WebRequest -Uri "$workshop_uri/siem_rules/AdversaryEmulation004.ndjson" -OutFile "$install_dir\AdversaryEmulation004.ndjson"    
+Invoke-WebRequest -Uri "$workshop_uri/siem_rules/AdversaryEmulation001.ndjson" -OutFile "$install_dir\AdversaryEmulation001.ndjson"
+Invoke-WebRequest -Uri "$workshop_uri/siem_rules/AdversaryEmulation002.ndjson" -OutFile "$install_dir\AdversaryEmulation002.ndjson"
+Invoke-WebRequest -Uri "$workshop_uri/siem_rules/AdversaryEmulation003.ndjson" -OutFile "$install_dir\AdversaryEmulation003.ndjson"
+Invoke-WebRequest -Uri "$workshop_uri/siem_rules/AdversaryEmulation004.ndjson" -OutFile "$install_dir\AdversaryEmulation004.ndjson"
 
 # Enable Siem Signal Rules
 Write-Output "Enabling SIEM Rules"
